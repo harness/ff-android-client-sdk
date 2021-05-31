74d98ebed96b384154cdd982aef14ced3a4b8a6d
Milos Vasic <milos.vasic@harness.io>
2021-05-31 14:53:13 +0200

    FFM-908 - Metrics test - Init SDK with the configuration.

--- cfsdk/src/test/java/io/harness/cfsdk/CfClientMetricsTest.java
+++ cfsdk/src/test/java/io/harness/cfsdk/CfClientMetricsTest.java
@@ -2,10 +2,18 @@ package io.harness.cfsdk;
 
 import android.content.Context;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import io.harness.cfsdk.cloud.factories.CloudFactory;
+import io.harness.cfsdk.cloud.model.Target;
 import io.harness.cfsdk.logging.CfLog;
+import io.harness.cfsdk.mock.MockedCloudFactory;
 
 public class CfClientMetricsTest {
 
@@ -13,9 +21,11 @@ public class CfClientMetricsTest {
     Context context;
 
     private final String logTag;
+    private final CloudFactory cloudFactory;
 
     {
 
+        cloudFactory = new MockedCloudFactory();
         logTag = CfClientInitTest.class.getSimpleName();
     }
 
@@ -24,6 +34,51 @@ public class CfClientMetricsTest {
 
         CfLog.testModeOn();
 
+        final String mock = "mock";
+        final CfClient cfClient = new CfClient(cloudFactory);
+        final String apiKey = String.valueOf(System.currentTimeMillis());
 
+        final CfConfiguration cfConfiguration = new CfConfiguration(
+
+                mock,
+                mock,
+                true,
+                true,
+                10
+        );
+
+        final AtomicBoolean initOk = new AtomicBoolean();
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        cfClient.initialize(
+
+                context,
+                apiKey,
+                cfConfiguration,
+                new Target().identifier("target"),
+
+                (info, result) -> {
+
+                    Assert.assertNotNull(info);
+                    Assert.assertNotNull(result);
+                    Assert.assertTrue(result.isSuccess());
+                    initOk.set(result.isSuccess());
+                    latch.countDown();
+                }
+        );
+
+        try {
+
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+
+            CfLog.OUT.e(logTag, e.getMessage(), e);
+        }
+
+        Assert.assertTrue(initOk.get());
+
+        // TODO: Test metrics
+
+        cfClient.destroy();
     }
 }
