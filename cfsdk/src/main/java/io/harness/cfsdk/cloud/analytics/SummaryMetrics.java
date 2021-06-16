11be6eae29573fe3986942ead133e80d5c4f03d8
Milos Vasic <milos.vasic@harness.io>
2021-06-16 14:59:08 +0200

    FFM-1061 - Summary metrics definition.

---
+++ cfsdk/src/main/java/io/harness/cfsdk/cloud/analytics/SummaryMetrics.java
@@ -0,0 +1,45 @@
+package io.harness.cfsdk.cloud.analytics;
+
+public class SummaryMetrics {
+
+    private String featureName;
+    private String variationValue;
+    private String variationIdentifier;
+
+    public SummaryMetrics(String featureName, String variationValue, String variationIdentifier) {
+
+        this.featureName = featureName;
+        this.variationValue = variationValue;
+        this.variationIdentifier = variationIdentifier;
+    }
+
+    public String getFeatureName() {
+
+        return featureName;
+    }
+
+    public void setFeatureName(String featureName) {
+
+        this.featureName = featureName;
+    }
+
+    public String getVariationIdentifier() {
+
+        return variationIdentifier;
+    }
+
+    public void setVariationIdentifier(String variationIdentifier) {
+
+        this.variationIdentifier = variationIdentifier;
+    }
+
+    public String getVariationValue() {
+
+        return variationValue;
+    }
+
+    public void setVariationValue(String variationValue) {
+
+        this.variationValue = variationValue;
+    }
+}
