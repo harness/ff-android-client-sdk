7276d62915c28627a798f964a5ce3a599fda817e
Milos Vasic <milos.vasic@harness.io>
2021-06-28 16:53:02 +0200

    FFM-1130 - Example app - Init.

--- example/src/main/java/io/harness/cfsdk/example/MainActivity.kt
+++ example/src/main/java/io/harness/cfsdk/example/MainActivity.kt
@@ -3,11 +3,15 @@ package io.harness.cfsdk.example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Toast
+
 import io.harness.cfsdk.*
 import io.harness.cfsdk.cloud.model.Target
 import io.harness.cfsdk.logging.CfLog
 
 class MainActivity : AppCompatActivity() {
+
+    private val logTag = MainActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val target = Target().identifier("Harness").name("Harness")
+
+        val remoteConfiguration = CfConfiguration.builder()
+            .enableAnalytics(true)
+//            .baseUrl("https://config.feature-flags.uat.harness.io/api/1.0")
+//            .eventUrl("https://event.feature-flags.uat.harness.io/api/1.0")
+//            .streamUrl("https://config.feature-flags.uat.harness.io/api/1.0/stream")
+            .enableStream(true)
+            .build()
+
+        CfClient.getInstance()
+            .initialize(
+
+                this,
+                Const.API_KEY,
+                remoteConfiguration,
+                target
+            ) { _, result ->
+
+                runOnUiThread {
+
+                    if (result.isSuccess) {
+
+
+                    } else {
+
+                        val e = result.error
+                        var msg = "Initialization error"
+                        e?.let { err ->
+
+                            err.message?.let { errMsg ->
+                                msg = errMsg
+                            }
+                            CfLog.OUT.e(logTag, msg, err)
+                        }
+                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 
     override fun onDestroy() {
