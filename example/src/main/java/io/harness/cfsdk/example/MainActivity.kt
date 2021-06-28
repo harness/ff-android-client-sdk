e6ca4d821d495fae348c8404a966eaf5dac16c6c
Milos Vasic <milos.vasic@harness.io>
2021-06-28 16:40:28 +0200

    FFM-1130 - Example app.

---
+++ example/src/main/java/io/harness/cfsdk/example/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.harness.cfsdk.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+}
