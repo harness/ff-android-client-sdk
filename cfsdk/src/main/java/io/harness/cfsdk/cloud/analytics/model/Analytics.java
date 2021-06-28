7875290bcd1da46917f59b0e8a2a7f828a0133da
Milos Vasic <milos.vasic@harness.io>
2021-06-28 16:00:46 +0200

    FFM-1130 - Equality and hash for Analytics.

--- cfsdk/src/main/java/io/harness/cfsdk/cloud/analytics/model/Analytics.java
+++ cfsdk/src/main/java/io/harness/cfsdk/cloud/analytics/model/Analytics.java
@@ -1,5 +1,7 @@
 package io.harness.cfsdk.cloud.analytics.model;
 
+import java.util.Objects;
+
 import io.harness.cfsdk.cloud.core.model.Variation;
 import io.harness.cfsdk.cloud.model.EventType;
 import io.harness.cfsdk.cloud.model.Target;
@@ -44,5 +46,29 @@ public class Analytics {
 
     public void setEventType(EventType eventType) {
         this.eventType = eventType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+
+            return false;
+        }
+
+        Analytics analytics = (Analytics) o;
+        return Objects.equals(target, analytics.target) &&
+                Objects.equals(variation.getName(), analytics.variation.getName()) &&
+                eventType == analytics.eventType;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(target, variation.getName(), eventType);
     }
 }
