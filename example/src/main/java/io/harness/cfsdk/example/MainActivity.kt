9d7ddc4c8eaa08240d5e6ea5ca504685dc49b3d4
Milos Vasic <milos.vasic@harness.io>
2021-06-28 16:47:47 +0200

    FFM-1130 - Example app - Target.

--- example/src/main/java/io/harness/cfsdk/example/MainActivity.kt
+++ example/src/main/java/io/harness/cfsdk/example/MainActivity.kt
@@ -3,12 +3,18 @@ package io.harness.cfsdk.example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import io.harness.cfsdk.*
+import io.harness.cfsdk.cloud.model.Target
+import io.harness.cfsdk.logging.CfLog
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val target = Target().identifier("Harness").name("Harness")
     }
 
     override fun onDestroy() {
