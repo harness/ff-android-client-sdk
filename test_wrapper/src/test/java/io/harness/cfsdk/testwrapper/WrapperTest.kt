51f258c5c181cf032f132b4a4084681bc689e439
Milos Vasic <milos.vasic@harness.io>
2021-07-21 13:27:03 +0200

    FFM-1005 - Prepare and log important test milestones.

--- test_wrapper/src/test/java/io/harness/cfsdk/testwrapper/WrapperTest.kt
+++ test_wrapper/src/test/java/io/harness/cfsdk/testwrapper/WrapperTest.kt
@@ -1,11 +1,20 @@
 package io.harness.cfsdk.testwrapper
 
+import io.harness.cfsdk.logging.CfLog
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class WrapperTest {
 
     private val server = WrapperServer()
+    private val tag = WrapperTest::class.simpleName
+
+    @Before
+    fun setup() {
+
+        CfLog.testModeOn()
+    }
 
     @Test
     fun testSDK() {
@@ -14,28 +23,39 @@ class WrapperTest {
 
             initLocalServer()
         )
+
+        CfLog.OUT.v(tag, "Local server is running")
+
         Assert.assertTrue(
 
             runTests()
         )
+
+        CfLog.OUT.v(tag, "Test have been executed")
+
         Assert.assertTrue(
 
             terminateLocalServer()
         )
+
+        CfLog.OUT.v(tag, "Local server has been shut down")
     }
 
     private fun initLocalServer(): Boolean {
 
+        CfLog.OUT.v(tag, "Initializing local server")
         return server.init()
     }
 
     private fun runTests(): Boolean {
 
+        CfLog.OUT.v(tag, "Running tests")
         return false
     }
 
     private fun terminateLocalServer(): Boolean {
 
+        CfLog.OUT.v(tag, "Shutting down local server")
         return server.shutdown()
     }
 }
