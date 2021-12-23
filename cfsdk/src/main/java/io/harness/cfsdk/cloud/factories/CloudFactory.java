f1c424bf63c96e21040909aee054fe962a3c33e0
Milos Vasic <milos85vasic@gmail.com>
2021-12-23 11:50:45 +0100

    FFM-2006 - Version increased to 1.0.6-SNAPSHOT.

--- cfsdk/src/main/java/io/harness/cfsdk/cloud/factories/CloudFactory.java
+++ cfsdk/src/main/java/io/harness/cfsdk/cloud/factories/CloudFactory.java
@@ -92,7 +92,7 @@ public class CloudFactory implements ICloudFactory {
     public ApiClient apiClient() {
 
         final ApiClient apiClient = new ApiClient();
-        apiClient.setUserAgent("android 1.0.5");
+        apiClient.setUserAgent("android 1.0.6-SNAPSHOT");
         String hostname = "UnknownHost";
         try {
 
