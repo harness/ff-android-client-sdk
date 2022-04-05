4cb9f59fe84a8c2feeca759a7c1bc96cc8088665
Milos Vasic <milos.vasic@harness.io>
2022-04-05 18:46:15 +0300

    [FFM-2641] Multidex dependency removed.

--- cfsdk/src/main/java/io/harness/cfsdk/cloud/factories/CloudFactory.java
+++ cfsdk/src/main/java/io/harness/cfsdk/cloud/factories/CloudFactory.java
@@ -92,7 +92,7 @@ public class CloudFactory implements ICloudFactory {
     public ApiClient apiClient() {
 
         final ApiClient apiClient = new ApiClient();
-        apiClient.setUserAgent("android 1.0.8");
+        apiClient.setUserAgent("android 1.0.9");
         String hostname = "UnknownHost";
         try {
 
