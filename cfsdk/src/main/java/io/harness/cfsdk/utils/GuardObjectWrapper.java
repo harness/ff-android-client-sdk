45b82a7ab7e74e31575998a7cbe5fade391f9901
Arek Szostak <arkadiusz.szostak@harness.io>
2022-10-24 16:26:14 +0100

    adding logging.

--- cfsdk/src/main/java/io/harness/cfsdk/utils/GuardObjectWrapper.java
+++ cfsdk/src/main/java/io/harness/cfsdk/utils/GuardObjectWrapper.java
@@ -2,16 +2,20 @@ package io.harness.cfsdk.utils;
 
 import java.util.concurrent.CountDownLatch;
 
+import io.harness.cfsdk.CfClient;
+import io.harness.cfsdk.logging.CfLog;
+
 /* Can be used to ensure method cannot be called on uninitialized object.*/
 public class GuardObjectWrapper {
 
     private Object object;
-
+    private String tag = GuardObjectWrapper.class.getSimpleName();
 
     private final CountDownLatch countDownLatch = new CountDownLatch(1);
 
     public Object get(){
         try {
+            CfLog.OUT.v(tag, "Awaiting for lock release");
             countDownLatch.await(); // wait for 0
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -20,7 +24,9 @@ public class GuardObjectWrapper {
     }
 
     public void set(Object newObject){
+
         // this will release other thread when 0
+        CfLog.OUT.v(tag, "Releasing lock "+ newObject.getClass().getSimpleName());
         this.object = newObject;
         countDownLatch.countDown();
     }
