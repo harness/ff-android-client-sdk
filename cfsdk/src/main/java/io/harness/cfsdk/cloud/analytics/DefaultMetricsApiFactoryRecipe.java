b34abb7919af293afb99cfcf473703e7dffa8ad4
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2023-03-27 12:19:35 +0100

    FFM-7037 - Fix HTTP header to use getEnvironmentIdentifier instead of getEnvironment
    What
    Minor fix to a previous commit use the readable id instead of the hash in the HTTP header

--- cfsdk/src/main/java/io/harness/cfsdk/cloud/analytics/DefaultMetricsApiFactoryRecipe.java
+++ cfsdk/src/main/java/io/harness/cfsdk/cloud/analytics/DefaultMetricsApiFactoryRecipe.java
@@ -49,7 +49,7 @@ public class DefaultMetricsApiFactoryRecipe implements MetricsApiFactoryRecipe {
 
             apiClient.addDefaultHeader("Hostname", hostname);
             apiClient.addDefaultHeader("Harness-SDK-Info", "Android " + ANDROID_SDK_VERSION + " Client");
-            apiClient.addDefaultHeader("Harness-EnvironmentID", authInfo.getEnvironment());
+            apiClient.addDefaultHeader("Harness-EnvironmentID", authInfo.getEnvironmentIdentifier());
             apiClient.addDefaultHeader("Harness-AccountID", authInfo.getAccountID());
             metricsAPI.setApiClient(apiClient);
         }
