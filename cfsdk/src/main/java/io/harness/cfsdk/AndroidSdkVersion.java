a0605c0e64e73ceb4ecb89217377eebbfdc0414f
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2024-01-29 16:59:56 +0000

    FFM-10521 - Refactor init/auth/stream/polling code
    What
    TBD

--- cfsdk/src/main/java/io/harness/cfsdk/AndroidSdkVersion.java
+++ cfsdk/src/main/java/io/harness/cfsdk/AndroidSdkVersion.java
@@ -1,5 +1,5 @@
 package io.harness.cfsdk;
 
 public class AndroidSdkVersion {
-    public static final String ANDROID_SDK_VERSION = "1.2.4";
+    public static final String ANDROID_SDK_VERSION = "2.0.0";
 }
